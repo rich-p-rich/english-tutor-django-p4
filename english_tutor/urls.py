24abe365acffa4be5598394ca350d424af487e9b
Rich Ricot <richricot@gmail.com>
2024-06-20 21:26:05 +0000

    Fix URL patterns in urls.py

--- english_tutor/urls.py
+++ english_tutor/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from home.views import my_home
+from django.urls import path, include
+from home import views as my_home
 
 
 urlpatterns = [
+    path('home/', home_views.my_home, name='home'),
     path('admin/', admin.site.urls),
-    path('home/', my_home, name='home'),
-
 ]
