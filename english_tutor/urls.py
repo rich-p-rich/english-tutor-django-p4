6e7d49375fe988f5e5dbd6829e0bc6f60691f295
Rich Ricot <richricot@gmail.com>
2024-06-21 08:01:11 +0000

    Correct URL patterns in urls.py

--- english_tutor/urls.py
+++ english_tutor/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home import views as my_home
+from home import views as home_views
 
 
 urlpatterns = [
