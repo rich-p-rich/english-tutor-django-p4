03db0791fce66d7e29140978a87d25ad7c7cc667
Rich Ricot <richricot@gmail.com>
2024-06-28 18:20:37 +0000

    Fix views and urls for home and appointments

--- english_tutor/urls.py
+++ english_tutor/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("appointments.urls"), name='appointments-urls'), 
-    path("", include("home.urls"), name='home-urls'),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path('appointments/', include("appointments.urls"), name='appointments-urls'), 
+    path('homepage/', include("home.urls"), name='home-urls'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-class HomePage(generic.TemplateView):
-    """
-    Renders the Home Page index.html
-    """
-    template_name = 'home/index.html'
+class HomePage(TemplateView):
+    template_name = 'home/home.html'
+
+#class HomePage(generic.TemplateView):
+ #   """
+  #  Renders the Home Page index.html
+   # """
+    #template_name = 'home/index.html'
