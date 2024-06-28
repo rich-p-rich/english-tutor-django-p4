e436f222ad98978aa412517140ad76c50f694787
Rich Ricot <richricot@gmail.com>
2024-06-28 18:36:44 +0000

    Add initial HTML with placeholder text to base

--- home/views.py
+++ home/views.py
@@ -4,10 +4,4 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class HomePage(TemplateView):
-    template_name = 'home/home.html'
-
-#class HomePage(generic.TemplateView):
- #   """
-  #  Renders the Home Page index.html
-   # """
-    #template_name = 'home/index.html'
+    template_name = 'home/index.html'
