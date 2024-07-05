15fe98a2b6d8a84c2b4b5b1f20c52ac94e10ab20
Richard Ricot <richricot@gmail.com>
2024-07-05 19:02:01 +0000

    Update appointments views and URLS to display form

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,6 +2,5 @@ from . import views
 from django.urls import path   
 
 urlpatterns = [
-    path('appointments/', book_appointment, name='appointments'),
-    path('appointment-confirmation/', TemplateView.as_view(template_name='appointment_confirmation.html'), name='appointment_confirmation.html'),
+    path('', views.make_appointment, name='appointments'),
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Appointment
 from .forms import AppointmentForm
 
-
-def appointments(request):
+def make_appointment(request):
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('appointment_confirmation')
+            return redirect('appointment-confirmation')
     else:
         form = AppointmentForm()
-    return render(request, 'appointments.html', {'form': form})
+    return render(request, 'appointments/appointments.html', {'form': form})
