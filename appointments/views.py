b095cddc0155b5e88674d9cacafb63cfed8023e7
Richard Ricot <richricot@gmail.com>
2024-07-06 19:12:26 +0000

    Fix views to show appt details on confirmation page

--- appointments/views.py
+++ appointments/views.py
@@ -10,6 +10,9 @@ def make_appointment(request):
         form = AppointmentForm(request.POST)
         if form.is_valid():
             appointment = form.save()
+            request.session['appointment_email'] = appointment.email
+            request.session['appointment_date'] = str(appointment.meeting_date)
+            request.session['appointment_time'] = str(appointment.meeting_time)
             return redirect('confirmation')
     else:
         form = AppointmentForm()
@@ -17,6 +20,11 @@ def make_appointment(request):
 
 # View for appointment confirmation 
 def confirm_appointment(request):
-    return render(request, 'appointments/confirmation.html')
+    appointment_details = {
+        'email': request.session.get('appointment_email'),
+        'date': request.session.get('appointment_date'),
+        'time': request.session.get('appointment_time')
+    }
+    return render(request, 'appointments/confirmation.html', {'appointment': appointment_details})
 
 # View for change or cancel a call 
