4801474ad9731e4401e466a3338a704ead2378cb
Richard Ricot <richricot@gmail.com>
2024-07-07 19:47:09 +0000

    Fix views to display booked appointments with appt ID

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.make_appointment, name='appointments'),
     path('confirmation/', views.confirm_appointment, name='confirmation'),
-    path('change/', views.change_appointments, name='change'),
+    path('change/', views.change_appointment, name='change'),
+    path('change/', views.change_appointment, name='change_appointment'),
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Appointment
-from .forms import AppointmentForm, SearchAppointmentsForm
+from .forms import AppointmentForm, SearchAppointmentsForm, ChangeAppointmentForm
 from django.views.decorators.csrf import csrf_protect
 
 @csrf_protect
@@ -28,27 +28,36 @@ def confirm_appointment(request):
     return render(request, 'appointments/confirmation.html', {'appointment': appointment_details})
 
 # View for searching a booked call based on surname and email 
-def change_appointments(request):
-    form = SearchAppointmentsForm()
+def search_appointments(request):
+    search_form = SearchAppointmentsForm()
     appointments = None
+
     if request.method == 'POST':
-        form = SearchAppointmentsForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            surname = form.cleaned_data['surname']
+        search_form = SearchAppointmentsForm(request.POST)
+        if search_form.is_valid():
+            email = search_form.cleaned_data['email']
+            surname = search_form.cleaned_data['surname']
             appointments = Appointment.objects.filter(email=email, surname=surname)
-    return render(request, 'appointments/change.html', {'form': form, 'appointments': appointments})
+
+    return render(request, 'appointments/search.html', {
+        'search_form': search_form,
+        'appointments': appointments
+    })
 
 # View for changing date and / time of a call
-def change_appointment(request, appointment_id):
+def edit_appointment(request, appointment_id):
     appointment = get_object_or_404(Appointment, id=appointment_id)
     if request.method == 'POST':
-        form = EditAppointmentForm(request.POST, instance=appointment)
-        if form.is_valid():
-            form.save()
-            return redirect('appointments')
+        change_form = ChangeAppointmentForm(request.POST, instance=appointment)
+        if change_form.is_valid():
+            change_form.save()
+            return render(request, 'appointments/confirmation.html', {'appointment': appointment})
     else:
-        form = EditAppointmentForm(instance=appointment)
-    return render(request, 'appointments/edit_appointment.html', {'form': form, 'appointment': appointment})
+        change_form = ChangeAppointmentForm(instance=appointment)
+    
+    return render(request, 'appointments/edit.html', {
+        'change_form': change_form,
+        'appointment': appointment
+    })
 
 # View for cancelling a call 
