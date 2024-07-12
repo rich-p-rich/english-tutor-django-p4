97a3a9d08ff84fd53b22470d895ef255d0989ffa
Richard Ricot <richricot@gmail.com>
2024-07-12 17:07:15 +0000

    Add conditional formatting to the cancel modal btn

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,1 +1,9 @@
-console.log("Life, The Universe and Everything!");
+document.addEventListener('DOMContentLoaded', function () {
+    var cancelModal = document.getElementById('cancelModal');
+    cancelModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget;
+        var appointmentId = button.getAttribute('data-appointment-id');
+        var modalBodyInput = cancelModal.querySelector('.modal-footer input[name="appointment_id"]');
+        modalBodyInput.value = appointmentId;
+    });
+});
