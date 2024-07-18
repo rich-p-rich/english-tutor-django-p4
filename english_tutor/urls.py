beb0badee0aacf4cfcfb24945c0a6e61b199eaaa
Richard Ricot <richricot@gmail.com>
2024-07-18 18:07:43 +0000

    Add register & login/out links to URLs & base.html

--- english_tutor/urls.py
+++ english_tutor/urls.py
@@ -23,6 +23,7 @@ from home.views import HomePage
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
     path('appointments/', include("appointments.urls"), name='appointments-urls'), 
     path('homepage/', include("home.urls"), name='home-urls'),
     path('', HomePage.as_view(), name='homepage'), 
