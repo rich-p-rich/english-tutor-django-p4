95d3ad2fdb924a67bd28ffe63e4cf50151c2f742
Richard Ricot <richricot@gmail.com>
2024-07-21 19:21:09 +0000

    Revert to basic sign-in page to focus on MVP

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -12,11 +12,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Toggle show password functionality on log-in page 
 
-function togglePassword () {
-    var x = document.getElementById("passwordInput")
-    if (x.type === "password") {
-        x.type = "text";
-    } else {
-      x.type = "password";
-    }
-}
+//function togglePassword () {
+//   var x = document.getElementById("passwordInput")
+//  if (x.type === "password") {
+//      x.type = "text";
+//  } else {
+//    x.type = "password";
+//  }
+//} 
+
+      const togglePassword = document
+            .querySelector('#togglePassword');
+        const password = document.querySelector('#password');
+        togglePassword.addEventListener('click', () => {
+            // Toggle the type attribute using
+            // getAttribure() method
+            const type = password
+                .getAttribute('type') === 'password' ?
+                'text' : 'password';
+            password.setAttribute('type', type);
+            // Toggle the eye and bi-eye icon
+            this.classList.toggle('bi-eye');
+        });
