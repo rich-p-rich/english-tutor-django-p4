3f6b25ddd8fb68ff7ca7fc1a717a3add11e8f552
Richard Ricot <richricot@gmail.com>
2024-07-24 20:59:18 +0000

    Add 'level' field to games&exercises -> models

--- games_and_exercises/models.py
+++ games_and_exercises/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class QuizQuestion(models.Model):
     question_text = models.CharField(max_length=255)
     correct_choice = models.CharField(max_length=50)
+    level = models.CharField(max_length=10, default='TBC')
+    # The level field categorizes the questions by difficulty
+    # The level is set to TBC(to be confirmed) as a default placeholder text
 
     def __str__(self):
         return self.question_text
